8c4c6c341e0fa5babab10b9e5e6ec6360accee21
William Andry <williamandrysm98@gmail.com>
2024-07-13 14:26:39 -0600

    Lamado de funciones usando tags html, llamado y creacion de arraysm,2 Formas de pasar valores(parametros), evitar comportamiento por default de boton submit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Header from './componentes/Header/Header.js';
 import Formulario from './componentes/Formulario/Formulario.js';
 
-//prueba
 function App() {
   return (
     <div >
--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,16 +1,27 @@
 import "./Formulario.css"
 import CampoTexto from "../CampoTexto"
-
+import ListaOpciones from "../ListaOpciones"
+import Boton from "../Boton"
 
 const Formulario = () => {
+
+    const manejarEnvio = (e) => {
+        e.preventDefault()
+        console.log("Manejar Envio",e);
+    }
+
     return  <section className="formulario">
-                <form>
-                    <h2>Rellena el formulario para crear el colaborador.</h2>
-                    <CampoTexto titulo="Nombre" placeholder="Ingresar nombre"/>
-                    <CampoTexto titulo="Puesto" placeholder="Ingresar puesto"/>
-                    <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto"/>
-                </form>
-            </section>
+        <form onSubmit= {manejarEnvio}> 
+            <h2>Rellena el formulario para crear el colaborador.</h2>
+            <CampoTexto titulo="Nombre" placeholder="Ingresar nombre"/>
+            <CampoTexto titulo="Puesto" placeholder="Ingresar puesto"/>
+            <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto"/>
+            <ListaOpciones/>
+            <Boton>
+                Crear
+            </Boton> 
+        </form>
+    </section>
 }
 
 export default Formulario
